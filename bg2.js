b8d505d22d731aeb9a04cd496b098d2994456954
Ravindu33 <105471474+Ravindu33@users.noreply.github.com>
2023-01-18 11:15:58 +0530

    Update and rename bg.js to bg2.js

--- bg2.js
+++ bg2.js
@@ -1,11 +1,11 @@
 
 var x = document.getElementById("feed-video-element");
-Ic = `<svg xmlns="http://www.w3.org/2000/svg"height="24px" style="position:fixed;top:50px;right:60px;" onclick ="ON()" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
+Ic = `<svg xmlns="http://www.w3.org/2000/svg"height="24px" style="position: absolute;top:50px;right:60px;" onclick ="ON()" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"/></svg>
 `;
 
 function go() {
 document.body.innerHTML += Ic;
-di = `<div id="Cc" style="display:none;background-color:black;color:white;width: 100%;height: 50%; position:fixed;bottom:0px;left:0px;border:2px solid pink;border-radius:10px;"></div>`;
+di = `<div id="Cc" style="display:none;background-color:black;color:white;width: 100%;height: 50%; position: absolute;bottom:0px;left:0px;border:2px solid pink;border-radius:10px;"></div>`;
 document.body.innerHTML += di;
 }
 
